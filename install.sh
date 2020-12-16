593ed8f0fe32636d5ef51ef1af6ee98e24b4c88a
icammaci <59759267+icammaci@users.noreply.github.com>
2020-12-16 16:20:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,61 +1,40 @@
-apt update
-
-apt upgrade
-
-apt install
-
-apt update && apt upgrade
-
+apt install ffmpeg 
 apt install wget
-
-apt install ffmpeg
-
 apt install nodejs
-
-apt install tesseract
-
+apt install npm
 npm i -g cwebp
-
-npm i -g ytdl 
-
+npm i node-tesseract-ocr
+npm i -g ytdl
 npm i
-
 npm i got
 
-Pkg install ffmpeg 
-
-Pkg install wget
-
-Pkg install nodejs
-
-Pkg install npm
-
+pkg install ffmpeg 
+pkg install wget
+pkg install nodejs
+pkg install npm
 npm i -g cwebp
-
 npm i node-tesseract-ocr
-
 npm i -g ytdl
-
 npm i
-
 npm i got
 
 apt-get install ffmpeg 
-
 apt-get install wget
-
 apt-get install nodejs
-
 apt-get install npm
-
 npm i -g cwebp
-
 npm i node-tesseract-ocr
-
 npm i -g ytdl
-
 npm i
-
 npm i got
 
+sudo apt-get install ffmpeg 
+sudo apt-get install wget
+sudo apt-get install nodejs
+sudo apt-get install npm
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
 node index.js
